22643c88c8e04996e0a732be778187afac8e6c51
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-04-16 23:07:42 -0600

    added form for new

--- client/src/components/NewItem.js
+++ client/src/components/NewItem.js
@@ -1,9 +1,93 @@
-import React from "react"
+import React from 'react';
+import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
 
-const NewItem = (props) => (
-  <div>
-    <h2>new item form</h2>
-  </div>
-)
+class NewItem extends React.Component {
+  state = {
+    name: '',
+    image: '',
+    description: '',
+    likes: '',
+  };
+  // const [name, setName] = useState('')
+  // const [image, setImage] = useState('')
+  // const [description, setDescription] = useState('')
+  // const [likes, setLikes] = useState('')
 
-export default NewItem
+  // const item = {item: item, image: image, description: description, likes: likes}
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post(`/api/items`, this.state)
+      .then((res) => {
+        this.setState({ state: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      ...this.state,
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { name, image, description, likes } = this.state;
+    return (
+      <>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            {/* <Form.Label>Name </Form.Label> */}
+            <Form.Input
+              placeholder="Name"
+              type="input"
+              name="name"
+              required
+              value={name}
+              onChange={this.handleChange}
+              required
+            />
+            {/* <Form.Label>Image </Form.Label> */}
+            <Form.Input
+              placeholder="Image"
+              type="input"
+              name="image"
+              required
+              value={image}
+              onChange={this.handleChange}
+              required
+            />
+            {/* <Form.Label>Description: </Form.Label> */}
+            <Form.Input
+              placeholder="Description"
+              type="input"
+              name="description"
+              required
+              value={description}
+              onChange={this.handleChange}
+              required
+            />
+            {/* <Form.Label>Likes </Form.Label> */}
+            <Form.Input
+              placeholder="Likes"
+              type="input"
+              name="likes"
+              required
+              value={likes}
+              onChange={this.handleChange}
+              required
+            />
+          </Form.Group>
+          <Button color='blue'>Submit</Button>
+        </Form>
+      </>
+    );
+  }
+}
+
+export default NewItem;
