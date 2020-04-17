fbaf9f7eac4029ce791b58aa39b15ec55b4dd494
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-04-16 22:08:42 -0600

    fixed no match

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Items from "./components/Items";
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import NewItem from "./components/NewItem"
+import NoMatch from "./components/NoMatch"
 
 const App = () => (
   <>
@@ -14,8 +15,8 @@ const App = () => (
     <Route exact path="/" component={Home} />
     <Route exact path="/items" component={Items} />
     <Route exact path="/new_item" component={NewItem}/>
+    <Route component={NoMatch} />
     </Switch>
-    
   </Container>
   </>
 )
--- client/src/components/Nomatch.js
+++ client/src/components/Nomatch.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const NoMatch = () => (
   <div>
-    <h2>Ruh Roh Error:404 Page Not Found</h2>
+    <h2>Ruh Roh,</h2>
+    <h2> Error:404 </h2>
+    <h2>Page Not Found</h2>
   </div>
 );
 
